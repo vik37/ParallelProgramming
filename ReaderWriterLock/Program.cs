21dcee1360a216fb57caedd9c07344b2c2195b8a
vik37 <vik.zafirovski@gmail.com>
2024-12-07 23:13:58 +0100

    Read Write Locks

---
+++ ReaderWriterLock/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ReaderWriterLock
+{
+	internal class Program
+	{
+		static ReaderWriterLockSlim padLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+		static Random rand = new Random();
+
+		static void Main(string[] args)
+		{
+			int x = 0;
+			var tasks = new List<Task>();
+			for (int i = 0; i < 10; i++)
+			{
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					//padLock.EnterReadLock();
+					//padLock.EnterReadLock();
+
+					padLock.EnterUpgradeableReadLock();
+
+					if (i % 2 == 0)
+					{
+						padLock.EnterWriteLock();
+						x = 123;
+						padLock.ExitWriteLock();
+					}
+
+					Console.WriteLine($"Enter read lock x = {x}");
+					Thread.Sleep(5000);
+
+					//padLock.ExitReadLock();
+					//padLock.ExitReadLock();
+
+					padLock.ExitUpgradeableReadLock();
+
+					Console.WriteLine($"Exited read lock x = {x}");
+				}));
+			}
+
+			try
+			{
+				Task.WaitAll(tasks.ToArray());
+			}
+			catch (AggregateException ae)
+			{
+				ae.Handle(e =>
+				{
+					Console.WriteLine(e);
+					return true;
+				});
+			}
+
+			while (true)
+			{
+				Console.ReadKey();
+				padLock.EnterWriteLock();
+				Console.WriteLine("Write lock required!");
+				int newValue = rand.Next();
+				x = newValue;
+				Console.WriteLine($"Set x = {x}");
+				padLock.ExitWriteLock();
+				Console.WriteLine("Write lock released!!!");
+			}
+		}
+	}
+}
--- Synchronization_CriticalSection/Program.cs
+++ Synchronization_CriticalSection/Program.cs
@@ -24,6 +24,12 @@
 			//Interlocked.Add(ref balance, -amount);
 			Balance -= amount;
 		}
+
+		public void Transfer(BankAccount where, int amount)
+		{
+			Balance -= amount;
+			where.Balance += amount;
+		}
 	}
 
 	internal class Program
@@ -32,10 +38,69 @@
 		{
 			var tasks = new List<Task>();
 			var ba = new BankAccount();
-			SpinLock sl = new SpinLock(true);
+			var ba2 = new BankAccount();
 
+			Mutex mutex = new Mutex();
+			Mutex mutex2 = new Mutex();
 			for (int i = 0; i < 10; i++)
 			{
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					for (int j = 0; j < 1000; j++)
+					{
+						bool haveLock = mutex.WaitOne();
+						try
+						{
+							ba.Deposit(1);
+						}
+						finally
+						{
+							if (haveLock)
+								mutex.ReleaseMutex();
+						}
+					}
+				}));
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					for (int j = 0; j < 1000; j++)
+					{
+						bool haveLock = mutex2.WaitOne();
+						try
+						{
+							//ba.Withdrow(100);
+							ba2.Deposit(1);
+						}
+						finally
+						{
+							if (haveLock)
+								mutex2.ReleaseMutex();
+						}
+					}
+				}));
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					for (int j = 0; j < 1000; j++)
+					{
+						bool haveLocks = WaitHandle.WaitAll(new[] { mutex, mutex2 });
+						try
+						{
+							ba.Transfer(ba2, 1);
+						}
+						finally
+						{
+							if (haveLocks)
+							{
+								mutex.ReleaseMutex();
+								mutex2.ReleaseMutex();
+							}
+						}
+					}
+				}));
+			}
+
+			#region Without Lock
+				//for (int i = 0; i < 10; i++)
+				//{
 				//for (int j = 0; j < 1000; j++)
 				//{
 				//	ba.Deposit(100);
@@ -44,45 +109,51 @@
 				//{
 				//	ba.Withdrow(100);
 				//}
+			#endregion
 
-				tasks.Add(Task.Factory.StartNew(() =>
-				{
-					for (int j = 0; j < 1000; j++)
-					{
-						bool locktaken = false;
-						try
-						{
-							sl.Enter(ref locktaken);
-							ba.Deposit(100);
-						}
-						finally
-						{
-							if (locktaken)
-								sl.Exit();
-						}
-					}
-				}));
-				tasks.Add(Task.Factory.StartNew(() =>
-				{
-					bool lockTaken = false;
-					for (int j = 0; j < 1000; j++)
-					{
-						try
-						{
-							sl.Enter(ref lockTaken);
-							ba.Withdrow(100);
-						}
-						finally
-						{
-							if (lockTaken)
-								sl.Exit();
-						}
-					}
-				}));				
-			}
+			#region SpinLock
+				//SpinLock sl = new SpinLock(true);
+
+				//	tasks.Add(Task.Factory.StartNew(() =>
+				//	{
+				//		for (int j = 0; j < 1000; j++)
+				//		{
+				//			bool locktaken = false;
+				//			try
+				//			{
+				//				sl.Enter(ref locktaken);
+				//				ba.Deposit(100);
+				//			}
+				//			finally
+				//			{
+				//				if (locktaken)
+				//					sl.Exit();
+				//			}
+				//		}
+				//	}));
+				//	tasks.Add(Task.Factory.StartNew(() =>
+				//	{
+				//		bool lockTaken = false;
+				//		for (int j = 0; j < 1000; j++)
+				//		{
+				//			try
+				//			{
+				//				sl.Enter(ref lockTaken);
+				//				ba.Withdrow(100);
+				//			}
+				//			finally
+				//			{
+				//				if (lockTaken)
+				//					sl.Exit();
+				//			}
+				//		}
+				//	}));				
+				//}
+			#endregion
 
 			Task.WaitAll(tasks.ToArray());
-			Console.WriteLine($"Final balance is {ba.Balance}");
+			Console.WriteLine($"Final balance ba is {ba.Balance}");
+			Console.WriteLine($"Final balance ba2 is {ba2.Balance}");
 		}
 	}
 }
