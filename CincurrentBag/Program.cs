7460d03486db3600eaa5714f7f8eaff0f0f990e5
vik37 <vik.zafirovski@gmail.com>
2024-12-21 19:09:32 +0100

    Concurrent Stack, Bag - Producer Consumer Pattern

---
+++ CincurrentBag/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace CincurrentBag
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var bag = new ConcurrentBag<int>();
+			var tasks = new List<Task>();
+			for (int i = 0; i < 10; i++)
+			{
+				var i1 = i;
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					bag.Add(i1);
+					Console.WriteLine($"{Task.CurrentId} has added {i1}");
+					int result;
+					if (bag.TryPeek(out result))
+					{
+						Console.WriteLine($"{Task.CurrentId} has peaked the value {result}");
+					}
+				}));
+			}
+
+			Task.WaitAll(tasks.ToArray());
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Producer_Consumer_Pattern/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+
+namespace Producer_Consumer_Pattern
+{
+	internal class Program
+	{
+		static BlockingCollection<int> messages 
+			= new BlockingCollection<int>(new ConcurrentBag<int>(), 10);
+
+		static CancellationTokenSource cts = new CancellationTokenSource();
+
+		static Random random = new Random();
+
+		static void ProducerAndConsumer()
+		{
+			var producer = Task.Factory.StartNew(RunProducer);
+			var consumer = Task.Factory.StartNew(RunConsumer);
+
+			try
+			{
+				Task.WaitAll(new[] { producer, consumer });
+			}
+			catch (AggregateException ae)
+			{
+				ae.Handle(e => true);
+			}
+		}
+
+		static void Main()
+		{
+			Task.Factory.StartNew(ProducerAndConsumer, cts.Token);
+
+			Console.ReadKey();
+			cts.Cancel();
+		}
+
+		private static void RunProducer()
+		{
+			while (true)
+			{
+				cts.Token.ThrowIfCancellationRequested();
+				int i = random.Next(100);
+				messages.Add(i);
+				Console.WriteLine($"+{i}\t");
+				Thread.Sleep(random.Next(1000));
+			}
+		}
+
+		private static void RunConsumer()
+		{
+			foreach (var item in messages.GetConsumingEnumerable())
+			{
+				cts.Token.ThrowIfCancellationRequested();
+				Console.WriteLine($"-{item}\t");
+				Thread.Sleep(random.Next(1000));
+			}
+		}
+	}
+}
